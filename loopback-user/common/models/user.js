a323b0467f36aa9beee0d3dcdea4d280f79deeb7
yzsunlei <846990846@qq.com>
2017-02-05 15:49:27 +0800

    update the model

--- loopback-user/common/models/user.js
+++ loopback-user/common/models/user.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(User) {
+  User.sayHi = function(callback) {
+    callback(null, 'Hi');
+  };
 
+  User.remoteMethod(
+    'sayHi',
+    {
+      'accepts': [],
+      'returns': [
+        {'arg': 'result', 'type': 'string'}
+      ],
+      'http': {
+        'verb': 'get',
+        'path': '/say-hi'
+      }
+    }
+  );
 };
